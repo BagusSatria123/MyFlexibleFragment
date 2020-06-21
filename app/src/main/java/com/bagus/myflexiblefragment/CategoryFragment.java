7ec5ef0544b392bdf64cd4f612fddabda276aec2
BagusSatria123 <satriabagusputra123@gmail.com>
2020-06-21 13:23:09 +0700

    menambahkan fragment agar bisa flexible fragment di satu activity

---
+++ app/src/main/java/com/bagus/myflexiblefragment/CategoryFragment.java
@@ -0,0 +1,38 @@
+package com.bagus.myflexiblefragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+    public CategoryFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_detail_category){
+        }
+    }
+}
--- app/src/main/java/com/bagus/myflexiblefragment/HomeFragment.java
+++ app/src/main/java/com/bagus/myflexiblefragment/HomeFragment.java
@@ -2,11 +2,15 @@ package com.bagus.myflexiblefragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -14,7 +18,7 @@ import android.view.ViewGroup;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,5 +64,27 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_category) {
+            CategoryFragment mCategoryFragment = new CategoryFragment();
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
     }
 }
